477ccfe92effbb72bc793067b1310f0d5da582e5
gildas <gildas.garcia@gmail.com>
2017-02-24 13:55:43 +0100

    review

--- src/elementsCountIs.js
+++ src/elementsCountIs.js
@@ -1,16 +1,16 @@
 import { Condition } from 'selenium-webdriver';
 import getWebElements from './getWebElements';
 
-export const checkElementsCountIsFactory = getWebElementsImpl => (elementOrSelector, count, waitTimeout) =>
-    async (driver) => {
-        const elements = await getWebElementsImpl(elementOrSelector, driver, waitTimeout);
+export const checkElementsCountIsFactory = getWebElementsImpl =>
+    (elementOrLocator, count, waitTimeout) =>
+        async (driver) => {
+            const elements = await getWebElementsImpl(elementOrLocator, driver, waitTimeout);
 
-        return Promise.resolve(elements && elements.length === count);
-    };
+            return Promise.resolve(elements && elements.length === count);
+        };
 
-export const elementIsClickedFactory = getWebElementsImpl => (elementOrSelector, count, waitTimeout) => {
-    const checkElementsCountIs = checkElementsCountIsFactory(getWebElementsImpl);
-    return new Condition('until element is clicked', checkElementsCountIs(elementOrSelector, count, waitTimeout));
-};
+export const elementIsClickedFactory = checkElementsCountIs =>
+    (elementOrLocator, count, waitTimeout) =>
+        new Condition('until element is clicked', checkElementsCountIs(elementOrLocator, count, waitTimeout));
 
-export default elementIsClickedFactory(getWebElements);
+export default elementIsClickedFactory(checkElementsCountIsFactory(getWebElements));
