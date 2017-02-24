348ea151fbca79a70be19ca98239e6059f9362f1
gildas <gildas.garcia@gmail.com>
2017-02-24 13:51:38 +0100

    review

--- src/elementValueIs.js
+++ src/elementValueIs.js
@@ -1,16 +1,16 @@
 import { Condition } from 'selenium-webdriver';
 import getWebElement from './getWebElement';
 
-export const checkElementValueIsFactory = getWebElementImpl => (elementOrSelector, value, waitTimeout) =>
-    async (driver) => {
-        const element = await getWebElementImpl(elementOrSelector, driver, waitTimeout);
-        return element.getAttribute('value').then(t => t === value);
-    };
+export const checkElementValueIsFactory = getWebElementImpl =>
+    (elementOrLocator, value, waitTimeout) =>
+        async (driver) => {
+            const element = await getWebElementImpl(elementOrLocator, driver, waitTimeout);
+            return element.getAttribute('value').then(t => t === value);
+        };
 
-export const elementValueIsFactory = getWebElementImpl => (elementOrSelector, value, waitTimeout) => {
-    const checkElementValueIs = checkElementValueIsFactory(getWebElementImpl);
-    return new Condition('until element value is', checkElementValueIs(elementOrSelector, value, waitTimeout));
-};
+export const elementValueIsFactory = checkElementValueIs =>
+    (elementOrLocator, value, waitTimeout) =>
+        new Condition('until element value is', checkElementValueIs(elementOrLocator, value, waitTimeout));
 
-export default elementValueIsFactory(getWebElement);
+export default elementValueIsFactory(checkElementValueIsFactory(getWebElement));
 
