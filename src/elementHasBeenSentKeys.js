fefc34e1edb3d7a887e1c368f9306d8a28565735
gildas <gildas.garcia@gmail.com>
2017-02-24 13:54:53 +0100

    review

--- src/elementHasBeenSentKeys.js
+++ src/elementHasBeenSentKeys.js
@@ -1,31 +1,31 @@
 import { Condition } from 'selenium-webdriver';
 import getWebElement from './getWebElement';
 
-export const checkElementHasBeenSentKeysFactory = getWebElementImpl => (elementOrSelector, waitTimeout) =>
-    async (driver) => {
-        const element = await getWebElementImpl(elementOrSelector, driver, waitTimeout);
+export const checkElementHasBeenSentKeysFactory = getWebElementImpl =>
+    (elementOrLocator, waitTimeout) =>
+        async (driver) => {
+            const element = await getWebElementImpl(elementOrLocator, driver, waitTimeout);
 
-        const [isDisplayed, isEnabled, tagName] = await Promise.all([
-            element.isDisplayed(),
-            element.isEnabled(),
-            element.getTagName(),
-        ]);
+            const [isDisplayed, isEnabled, tagName] = await Promise.all([
+                element.isDisplayed(),
+                element.isEnabled(),
+                element.getTagName(),
+            ]);
 
-        if (!['input', 'textarea'].includes(tagName.toLowerCase())) {
-            throw new Error('Invalid element: must be an input or a textarea');
-        }
+            if (!['input', 'textarea'].includes(tagName.toLowerCase())) {
+                throw new Error('Invalid element: must be an input or a textarea');
+            }
 
-        if (!isDisplayed || !isEnabled) return null;
+            if (!isDisplayed || !isEnabled) return null;
 
-        return element
-                .sendKeys('')
-                .then(() => true)
-                .catch(() => false);
-    };
+            return element
+                    .sendKeys('')
+                    .then(() => true)
+                    .catch(() => false);
+        };
 
-export const elementHasBeenSentKeysFactory = getWebElementImpl => (elementOrSelector, waitTimeout) => {
-    const checkElementHasBeenSentKeys = checkElementHasBeenSentKeysFactory(getWebElementImpl);
-    return new Condition('until element has been sent keys', checkElementHasBeenSentKeys(elementOrSelector, waitTimeout));
-};
+export const elementHasBeenSentKeysFactory = checkElementHasBeenSentKeys =>
+    (elementOrLocator, waitTimeout) =>
+        new Condition('until element has been sent keys', checkElementHasBeenSentKeys(elementOrLocator, waitTimeout));
 
-export default elementHasBeenSentKeysFactory(getWebElement);
+export default elementHasBeenSentKeysFactory(checkElementHasBeenSentKeysFactory(getWebElement));
