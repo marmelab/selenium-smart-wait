50c22c6c8c8dac413dd8072bb3dd28ed37fe2535
ThieryMichel <thiery@marmelab.com>
2017-04-07 10:08:38 +0200

    use path.join

--- e2e/chromeWebDriver.js
+++ e2e/chromeWebDriver.js
@@ -1,8 +1,9 @@
 import webdriver from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
 import logging from 'selenium-webdriver/lib/logging';
+import path from 'path';
 
-const chromePath = `${__dirname}/../${process.env.SELENIUM_BROWSER_BINARY_PATH}`;
+const chromePath = path.join(__dirname, '/../', process.env.SELENIUM_BROWSER_BINARY_PATH);
 const service = new chrome.ServiceBuilder(chromePath).build();
 const DEFAULT_WAIT_TIMEOUT = 1000;
 
